6eac81673f6ee6f196e2e761be2d42c8fd6197c8
SoChigusa's MacBook Air <so.chigusa.pp@gmail.com>
2020-11-22 02:50:05 -0800

    Backup of atm.dat in homepage directory

--- update.py
+++ update.py
@@ -5,6 +5,7 @@ subprocess.run(['src/a.out'])
 import plot
 print('Now updating...')
 shutil.copy('plot.png', '/Users/SoChigusa/works/homepage/')
+shutil.copy('atm.dat', '/Users/SoChigusa/works/homepage/')
 shutil.copy('plot.png', '/Users/SoChigusa/works/sochigusa.bitbucket.org/')
 os.chdir('/Users/SoChigusa/works/sochigusa.bitbucket.org/')
 subprocess.run(['git','commit','-a','-m','"Auto commit by mental health update"'])
