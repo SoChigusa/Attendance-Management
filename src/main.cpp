9267973899fb31a0a994eaf6cb29c549c69ea565
So Chigusa <so.chigusa.pp@gmail.com>
2020-12-11 16:31:12 -0800

    atom-beautify

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,31 @@
+#include <time.h>
+
 #include <boost/algorithm/string.hpp>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <time.h>
 
-int main() {
+int main()
+{
 
-  std::string dstr, val1="0", val2="0";
+  std::string dstr, val1 = "0", val2 = "0";
 
   // date
   time_t timer = time(NULL) - 60 * 60 * 3;
   struct tm *date = localtime(&timer), input;
   std::cout << "Input date from today (n <= 0) (default: ";
-  std::cout << date->tm_year+1900 << "/"
-	    << date->tm_mon+1 << "/"
-	    << date->tm_mday;
+  std::cout << date->tm_year + 1900 << "/" << date->tm_mon + 1 << "/"
+            << date->tm_mday;
   std::cout << "): ";
   std::getline(std::cin, dstr);
-  if(dstr != "") {
+  if (dstr != "")
+  {
     // std::mktime not working properly on macOS arm 2020/12/11
     // strptime(dstr.c_str(), "%Y/%m/%d", &input);
     // input.tm_hour = 12;
     // timer = std::mktime(&input);
     int n = stoi(dstr);
-    timer += n*60*60*24;
+    timer += n * 60 * 60 * 24;
 
     // std::cout << input.tm_year+1900 << "/"
     // 	      << input.tm_mon+1 << "/"
@@ -44,10 +46,7 @@ int main() {
 
   // output to file
   std::ofstream ofs("atm.dat", std::ios::app);
-  ofs << serial << "\t"
-      << val1 << "\t"
-      << val2 << std::endl;
+  ofs << serial << "\t" << val1 << "\t" << val2 << std::endl;
 
   return 0;
-
 }
