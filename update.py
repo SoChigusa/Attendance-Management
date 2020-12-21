41da3ff69e75a0e27867b8ae579d4ddd6abddbb2
千草颯 <chigusa@hep-th.phys.s.u-tokyo.ac.jp>
2020-12-21 11:57:57 +0900

    Console input part rewritten in python

--- update.py
+++ update.py
@@ -1,7 +1,25 @@
+# Console input of workload/mood data
+import datetime
+date = datetime.date.today() - datetime.timedelta(hours=3)
+print('Input date (default: '+date.strftime('%Y/%m/%d')+')')
+strin = input('>> ')
+if(strin != ''):
+    date = datetime.datetime.strptime(strin, '%Y/%m/%d').date()
+dd = date - datetime.date(1970,1,1)
+x = dd.days
+print('How hard did you work? (rate by [0-3])')
+strin = input('>> ')
+y = int(strin)
+print('How was your mood? (rate by [0-2])')
+strin = input('>> ')
+z = int(strin)
+with open('atm.dat', mode='a') as f:
+    f.write(str(x)+'\t'+str(y)+'\t'+str(z)+'\n')
+
+# Plot and update
 import os
 import subprocess
 import shutil
-subprocess.run(['src/a.out'])
 import plot
 print('Now updating...')
 shutil.copy('plot.png', '/Users/SoChigusa/works/homepage/')
