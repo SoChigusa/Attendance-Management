efefeaa4a6668fe51ba0ca71d79542e476c036a6
So Chigusa <so.chigusa.pp@gmail.com>
2020-12-28 12:37:15 +0900

    "Auto commit by update.py"

--- update.py
+++ update.py
@@ -24,8 +24,6 @@ import plot
 print('Now updating...')
 subprocess.run(['git','commit','-a','-m','"Auto commit by update.py"'])
 subprocess.run(['git','push','origin','main'])
-shutil.copy('plot.png', '/Users/SoChigusa/works/homepage/')
-shutil.copy('atm.dat', '/Users/SoChigusa/works/homepage/')
 shutil.copy('plot.png', '/Users/SoChigusa/works/sochigusa.bitbucket.org/')
 os.chdir('/Users/SoChigusa/works/sochigusa.bitbucket.org/')
 subprocess.run(['git','commit','-a','-m','"Auto commit by mental health update"'])
